ead5bc439ca8876c6415566e2267f6e7566e16de
sreedhar <sreedharks2003@gmail.com>
2022-09-17 22:45:07 +0530

    final

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl ="https://uqerhibuqripzryleofp.supabase.co";
-const supabaseAnonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxZXJoaWJ1cXJpcHpyeWxlb2ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMzNTEyNjcsImV4cCI6MTk3ODkyNzI2N30.6swYfw6aJMmQkUTdiNa3Nu08D4F-16y5N_uXAb6pZeo";
+const supabaseUrl ="apirul";
+const supabaseAnonKey ="anonkey";
 
 
 export const supabase = createClient(supabaseUrl,supabaseAnonKey);
